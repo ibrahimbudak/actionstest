92a205fcc71704b09fb1c9c391a81e1ac0a1e4f3
Turkopat <154666382+Turkopat@users.noreply.github.com>
2024-02-12 11:03:02 +0300

    Update script.py

--- script.py
+++ script.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
         )
         response = client.create_security_group_rule(request)
         print("Response alindi")
-        print(response + "Merhaba")
+        print(response)
         __import__('os').environ['SG_RULE_ID'] = response.security_group_rule.id
         sg_id = __import__('os').getenv("SG_RULE_ID")
         print(sg_id)
